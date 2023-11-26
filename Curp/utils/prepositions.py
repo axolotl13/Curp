4963111e69a34ee45924124dcf3f7dc8800f2771
joker <geovannicinto@gmail.com>
2023-11-26 17:04:07 -0600

    Curp v2

---
+++ Curp/utils/prepositions.py
@@ -0,0 +1,26 @@
+"""
+Copyright (c) 2023 Geovanni Santamaria. All Rights Reserved.
+This is a list of prepositions, conjunctions, and contractions used by RENAPO
+to filter in the names and surnames.
+"""
+prep = [
+    "da",
+    "das",
+    "de",
+    "del",
+    "der",
+    "di",
+    "die",
+    "dd",
+    "el",
+    "la",
+    "los",
+    "las",
+    "le",
+    "les",
+    "mac",
+    "mc",
+    "van",
+    "von",
+    "y",
+]
