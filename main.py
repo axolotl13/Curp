5dda0e25c5e40eecbb9dbe77ee07ab2469521635
joker <geovannicinto@gmail.com>
2024-03-20 17:33:59 -0600

    Nuevo menu

--- main.py
+++ main.py
@@ -1,41 +1,31 @@
+from logging import error as lerror
+
 from generator import Generator
 
-
-def main():
-    Generator("Alberto", "Ñando", "Rodríguez", 12, 2, 1979, "H", "Yucatan").printer()
-    Generator(
-        "María Luisa", "Pérez", "Hernandez", 28, 1, 1999, "Mujer", "Queretaro"
-    ).printer()
-    Generator(
-        "Luis Enrique", "Romero", "Palazuelos", 30, 6, 1923, "Hombre", "Nuevo León"
-    ).printer()
-    Generator(
-        "Juan Jose", "D/amico", "Alvares", 1, 12, 1980, "Hombre", "Sinaloa"
-    ).printer()
-    Generator("Rocio", "Riva Palacio", "Cruz", 3, 8, 2001, "M", "Oaxaca").printer()
-    Generator(
-        "Carlos", "MC Gregor", "López", 13, 6, 1923, "Hombre", "Estado de México"
-    ).printer()
-    Generator(
-        "Ofelia", "Pedrero", "Dóminguez", 23, 10, 2010, "Mujer", "Veracruz"
-    ).printer()
-    Generator(
-        "Andres", "Ich", "Rodríguez", 20, 9, 2000, "H", "Baja California"
-    ).printer()
-    Generator("Luis", "Perez", "", 6, 10, 1967, "H", "Chihuahua").printer()
-    Generator("Alberto", "Oñate", "Rodríguez", 12, 1, 2017, "H", "Colima").printer()
-    Generator("Leticia", "Luna", "", 16, 12, 2003, "M", "Guerrero").printer()
-    Generator(
-        "Maria de los Angeles",
-        "Moreno",
-        "Sanchez",
-        19,
-        8,
-        1983,
-        "M",
-        "Baja California Sur",
-    ).printer()
-
-
 if __name__ == "__main__":
-    main()
+    try:
+        options = (
+            'Name(s): "José María": ',
+            "First surname: ",
+            "Second surname: ",
+            "Birth day: ",
+            "Birth month: ",
+            "Birt year: ",
+            "Gender: ",
+            "State: ",
+        )
+        data = tuple(map(input, options))
+        Generator(
+            data[0],
+            data[1],
+            data[2],
+            int(data[3]),
+            int(data[4]),
+            int(data[5]),
+            data[6],
+            data[7],
+        ).printer()
+    except KeyboardInterrupt as e:
+        lerror(f"Error: Se interrumpio la ejecución {e}")
+    except Exception as e:
+        lerror(f"Error: Se produjo una falla - {e}")
